ab38df5d4234e7d30c7efa5df65e35b57de5b306
Sitt Guruvanich <aekazitt@gmail.com>
2025-05-15 19:57:06 +0700

    fix: update dependency checks with or-gates

--- plugin/tylsp.lua
+++ plugin/tylsp.lua
@@ -1,6 +1,6 @@
 -- plugin/tylsp.lua --
 
-if not pcall(require, 'lspconfig') and not pcall(require, 'plenary') then
+if not pcall(require, 'lspconfig') or not pcall(require, 'plenary') then
   return
 end
 require('tylsp').setup()
