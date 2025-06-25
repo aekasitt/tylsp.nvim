ca1562bcc8e75634b8cd8daea83b8902496ed8f4
Sitt Guruvanich <aekazitt@gmail.com>
2025-06-26 00:44:32 +0700

    feat(build): notify users with stdout/stderr info

--- plugin/build.lua
+++ plugin/build.lua
@@ -1,12 +1,40 @@
 -- plugin/build.lua --
 
 local Job = require('plenary.job')
-Job:new({
-  args = { 'install', '--user', 'ty' },
-  command = 'pip',
-  on_exit = function(_, return_val)
-    if return_val ~= 0 then
-      print("Unable to install Tylsp's python dependencies")
-    end
-  end,
-}):start()
+
+if vim.fn.executable('ty') == 0 then
+  vim.schedule(function()
+    vim.notify('Installing astral-sh/ty for tylsp.nvim…', vim.log.levels.INFO)
+  end)
+  if vim.fn.executable('pip') == 0 then
+    vim.schedule(function()
+      vim.notify('Unable to locate pip command', vim.log.levels.ERROR)
+    end)
+    return
+  end
+  Job:new({
+    args = { 'install', '--user', 'ty' },
+    command = 'pip',
+    on_stdout = function(_, data)
+      if not data:match("^%s*Collecting") and not data:match('^%s*Using cached') then
+        vim.schedule(function()
+          vim.notify(data, vim.log.levels.INFO)
+        end)
+      end
+    end,
+    on_stderr = function(_, data)
+      if not data:match('^%s*%[notice%]') then
+        vim.schedule(function()
+          vim.notify(data, vim.log.levels.WARN)
+        end)
+      end
+    end,
+    on_exit = function(_, return_val)
+      if return_val ~= 0 then
+        vim.schedule(function()
+          vim.notify("Unable to install Tylsp's python dependencies", vim.log.levels.ERROR)
+        end)
+      end
+    end,
+  }):start()
+end
